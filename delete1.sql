65de0f2dba74db49b8b11451633a3d0e14efbdad
Sarath Wesley <wesleysarath1@gmail.com>
2021-06-16 21:41:39 +0530

    Adds solution files

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-delete from books_subjects where subject in (9);
+delete from books_subjects where subject in (select bs.subject from subjects s, books_subjects bs where bs.subject = s.id and s.name = "History");
 delete from subjects where name = "History";
